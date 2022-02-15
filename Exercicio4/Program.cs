27fa6133cd1cb6319e26b12dba19703bd0e616b6
Priscila Padilla Latance <priscila.latance@gmail.com>
2022-02-15 20:36:09 -0300

    Teste GFT

--- Exercicio4/Program.cs
+++ Exercicio4/Program.cs
@@ -7,13 +7,32 @@ namespace Exercicio4
     {
         static void Main(string[] args)
         {                          
-            int n = 0;                    
-             string[] number = new string[n];
-            for(n=0; n < number.Length; n++)
-            {
-            int value =int.Parse(ReadLine(n));
-            }
+            string[] number1 = ReadLine().Split(',');
+            string[] number2 = ReadLine().Split(',');            
 
-        }
+                try
+                {
+                    if (number1.Length != number2.Length)
+                    {
+                        throw new Exception("Array de tamanhos diferentes.");
+                    }
+                    else{
+                        int nValue = number1.Length;
+                        int[] value = new int[nValue];                    
+                        for(int n1 = 0; n1 < number1.Length-1; n1++){
+                            for(int n2 = 0; n2 < number2.Length-1; n2++){
+                                value[nValue] = Convert.ToInt32(number1[n1]) * Convert.ToInt32(number2[n2]);
+                                WriteLine($"{value[nValue]}");
+
+                            }
+                        }
+                    }
+                }
+                catch (Exception fault)
+                {
+                    WriteLine($"Erro:{fault.Message}");
+                }
+        
+        }           
     }
 }
